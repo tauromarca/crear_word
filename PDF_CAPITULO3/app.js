3fce2813026949dfdc1e824e4ac9faae29cbec5e
tauromarca <claudio.goldberg.m@gmail.com>
2026-04-30 16:51:14 -0400

    Add files via upload

--- PDF_CAPITULO3/app.js
+++ PDF_CAPITULO3/app.js
@@ -24,10 +24,10 @@
                 { nombre: "B. Cierres Perimetrales", p: rawData.b_ponderado, intervencion: getLabel("tipo_intervencion_perimetrales", rawData.tipo_intervencion_perimetrales) },
                 { nombre: "C. Techumbre", p: rawData.c_ponderado, intervencion: getLabel("tipo_intervencion_techumbre", rawData.tipo_intervencion_techumbre)},
                 { nombre: "D. Ascensores, Escaleras y/o Circulaciones", p: rawData.d_ponderado, intervencion: getLabel("tipo_intervencion_ascensores", rawData.tipo_intervencion_ascensores)},
-                { nombre: "E. Fachadas y/o Muros", p: rawData.e_ponderado, intervencion: getLabel("tipo_intervencion_fachadas", rawData.tipo_intervencion_fachadas) },
-                { nombre: "F. Sistemas de Iluminación", p: rawData.f_ponderado, intervencion: getLabel("tipo_intervencion_iluminacion", rawData.tipo_intervencion_iluminacion)},
-                { nombre: "G. Redes de Servicio", p: rawData.g_ponderado, intervencion: getLabel("tipo_intervencion_redes", rawData.tipo_intervencion_redes)},
-                { nombre: "K. Accesibilidad Universal", p: rawData.k_ponderado, intervencion: getLabel("tipo_intervencion_accesibilidad", rawData.tipo_intervencion_accesibilidad) }
+                { nombre: "E. Fachadas y/o Muros", p: rawData.e_ponderado, intervencion: getLabel("tipo_intervencion_fachada", rawData.tipo_intervencion_fachada) },
+                { nombre: "F. Sistemas de Iluminación", p: rawData.f_ponderado, intervencion: getLabel("tipo_intervencion_iluminaria", rawData.tipo_intervencion_iluminaria)},
+                { nombre: "G. Redes de Servicio", p: rawData.g_ponderado, intervencion: getLabel("Tipo_Intervencion_Redes_servicios", rawData.Tipo_Intervencion_Redes_servicios)},
+                { nombre: "K. Accesibilidad Universal", p: rawData.k_ponderado, intervencion: "No aplica" }
             ];
             partidas.sort((a, b) => parseFloat(b.p || 0) - parseFloat(a.p || 0));
             return partidas.map(item => ({
