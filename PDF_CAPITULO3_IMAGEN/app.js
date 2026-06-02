6d09c62b8dab160d6c0caddf4bbc97589f44099f
tauromarca <claudio.goldberg.m@gmail.com>
2026-06-02 14:51:05 -0400

    Update app.js

--- PDF_CAPITULO3_IMAGEN/app.js
+++ PDF_CAPITULO3_IMAGEN/app.js
@@ -1,5 +1,5 @@
-import { TemplateHandler } from "https://cdn.jsdelivr.net/npm/easy-template-x@7.2.4/+esm";
-//import { TemplateHandler } from "./easy-template-x.esm.js";
+//import { TemplateHandler } from "https://cdn.jsdelivr.net/npm/easy-template-x@7.2.4/+esm";
+import { TemplateHandler } from "./libs/easy-template-x.js";
 require([
     "esri/Map",
     "esri/views/MapView",
